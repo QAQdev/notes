2cd73d0ea0feceae3803caf9bf8c73fef7ed511e
kenshin <73159641+QAQdev@users.noreply.github.com>
2023-07-05 17:39:38 +0800

    cache

--- docs/Playground/NASA.md
+++ docs/Playground/NASA.md
@@ -20,45 +20,4 @@
 
     <div id="apod-image"></div>
 
-
-<script>
-const apiKey = 'NmJMd2hpUVJUbEJkWDlhNTdZQnJNZjU5S2lGWEMwN25kbHhicGxhRw==';
-const k = atob(apiKey);
-const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${k}`;
-
-function fetchData() {
-  const cachedData = localStorage.getItem('apodData');
-  if (cachedData) {
-    const data = JSON.parse(cachedData);
-    displayData(data);
-  } else {
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        localStorage.setItem('apodData', JSON.stringify(data));
-        console.log("first time");
-        displayData(data);
-      });
-  }
-}
-
-function displayData(data) {
-  
-  const img = document.createElement('img');
-  img.src = data.hdurl;
-  description = data.explanation;
-  document.querySelector('#apod-image').appendChild(img);
-
-  const title = document.querySelector('#apod-title');
-  title.textContent = data.title;
-
-  const date = document.querySelector('#apod-date');
-  date.textContent = "Shot on " + data.date;
-
-  const explanation = document.querySelector('#apod-desc');
-  explanation.textContent = data.explanation;
-}
-
-fetchData();
-
-</script>
+<script src="#/docs/js/playground/nasa.js"></script>
---
+++ docs/js/playground/nasa.js
@@ -0,0 +1,42 @@
+const apiKey = 'NmJMd2hpUVJUbEJkWDlhNTdZQnJNZjU5S2lGWEMwN25kbHhicGxhRw==';
+const k = atob(apiKey);
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${k}`;
+
+function fetchData() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const cachedData = localStorage.getItem('apodData' + year + month + day);
+  if (cachedData) {
+    const data = JSON.parse(cachedData);
+    displayData(data);
+  } else {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        localStorage.setItem('apodData', JSON.stringify(data));
+        console.log("first time");
+        displayData(data);
+      });
+  }
+}
+
+function displayData(data) {
+  
+  const img = document.createElement('img');
+  img.src = data.hdurl;
+  description = data.explanation;
+  document.querySelector('#apod-image').appendChild(img);
+
+  const title = document.querySelector('#apod-title');
+  title.textContent = data.title;
+
+  const date = document.querySelector('#apod-date');
+  date.textContent = "Shot on " + data.date;
+
+  const explanation = document.querySelector('#apod-desc');
+  explanation.textContent = data.explanation;
+}
+
+fetchData();
